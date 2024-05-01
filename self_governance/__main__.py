83cec3d029b1737a5afd864503193ddb1c8c79c4
Jiayu Yi <yijiayu@gmail.com>
2024-05-01 11:43:36 +0800

    Replace deprecated completions with chat

--- self_governance/__main__.py
+++ self_governance/__main__.py
@@ -1,20 +1,16 @@
 import os
 
 import httpx
-import openai
 import typer
 from jinja2 import Template
+from openai import OpenAI
 
 from .nation import Issue, Nation
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
 template = Template(
     """The following text describes the nation of {{ name }}:
 
 {{ bio }}
-
-Given the following scenario and options, which option is most suitable for the nation of {{ name }}?
 
 Scenario:
 {{ issue.text }}{% for option in issue.options %}
@@ -26,31 +22,42 @@ Answer: Option"""
 )
 
 
-def address_issue(nation: Nation, bio: str, issue: Issue) -> None:
+def address_issue(openai_client, nation: Nation, bio: str, issue: Issue) -> None:
     prompt = template.render(name=nation.name, bio=bio, issue=issue)
     print(prompt)
 
-    response = openai.Completion.create(
-        model="gpt-3.5-turbo-instruct",
-        prompt=prompt,
+    response = openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are the personification of a nation-state and are making policy decisions on its behalf. You will be given a description of the nation-state itself, a scenario  and a numbered list of options to choose from. Reply only with a single integer: the number of the option you believe is most suitable for the nation-state to take in that scenario.",
+            },
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+        temperature=1,
         max_tokens=1,
-        temperature=0.4,
-        best_of=5,
+        top_p=1,
         frequency_penalty=0,
         presence_penalty=0,
     )
-    option_index = int(response["choices"][0]["text"].strip()) - 1
+    print(response)
+    option_index = int(response.choices[0].message.content)
     chosen_option = issue.options[option_index]
 
     return nation.answer_issue(issue.id, chosen_option.id)
 
 
 def main(nation_name: str, nation_bio: str):
+    client = OpenAI()
     password = os.environ["NATIONSTATES_PASSWORD"]
     nation = Nation(nation_name, password, session_file="session.json")
     issues = nation.get_issues()
     for issue in issues:
-        print(address_issue(nation, nation_bio, issue))
+        print(address_issue(client, nation, nation_bio, issue))
     else:
         print("No issues to address")
 
